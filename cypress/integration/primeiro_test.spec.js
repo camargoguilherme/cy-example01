c30d84e9e75b87d275c720f1f6f5dce03a5b3b28
Guilherme Camargo <guilhermecamargo57@hotmail.com>
2021-03-23 23:00:49 -0300

    Implementado teste 'deve calcular o imposto devido' no cypress

--- cypress/integration/primeiro_test.spec.js
+++ cypress/integration/primeiro_test.spec.js
@@ -5,7 +5,7 @@ describe('Calculadora de imposto', () => {
         cy.get('[name=idade]').type('20');
         cy.get('[name=sexo]').select('M');
         cy.get('[name=salario]').type('2000');
-        // cy.get(`[value='Limpar campos']`).click();
+        cy.get(`[value='Limpar campos']`).click();
 
         cy.get('[name=nome]').should('have.text', '');
         cy.get('[name=idade]').should('have.text', '');
@@ -15,9 +15,20 @@ describe('Calculadora de imposto', () => {
 
     it('deve calcular o imposto devido', () => {
         // preencha todos os campos
+        cy.visit('https://andreendo.github.io/sample-ui-compras/index-2.html');
+        cy.get('[name=nome]').type('Guilherme Camargo');
+        cy.get('[name=idade]').type('25');
+        cy.get('[name=sexo]').select('M');
+        cy.get('[name=salario]').type('6000');
 
         // clique no botao calcular
+        cy.get(`[value='Calcular']`).click();
 
         // verifique (usando .should) a saída retornada com o calculo do imposto devido
+        cy.get('[name=salario]').invoke('val').then( salario => {
+            const percent = salario > 5000 ? 0.25 : 0.15;
+            cy.get('[id=divValorImposto]').should('have.text', `Valor a pagar de imposto: ${salario * percent}`);
+        })
+
     });
 });
